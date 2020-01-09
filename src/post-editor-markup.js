22ff562804e81c67b95f96e614395ac910017b63
Rehabescapi <jasonmlehmann@gmail.com>
2020-01-09 14:51:33 -0600

    Finished API mock

---
+++ src/post-editor-markup.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {savePost} from './api'
+function Editor({user}) {
+  const [isSaving, setIsSaving] = React.useState(false)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const {title, content, tags} = e.target.elements
+    setIsSaving(true)
+    savePost({
+      title: title.value,
+      content: content.value,
+      tags: tags.value.split(',').map(t => t.trim()),
+      authorId: user.id,
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="title-input">Title</label>
+      <input id="title-input" name="title" />
+
+      <label htmlFor="content-input">Content</label>
+      <textarea id="content-input" name="content" />
+
+      <label htmlFor="tags-input">Tags</label>
+      <textarea name="tags" id="tags-input" />
+
+      <button type="submit" disabled={isSaving}>
+        Submit
+      </button>
+    </form>
+  )
+}
+
+export {Editor}
