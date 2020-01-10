d98b966fd31b057edd299fa2064eb37eb7f2081d
Rehabescapi <jasonmlehmann@gmail.com>
2020-01-09 19:26:47 -0600

    rada

--- src/__tests__/tdd-markup.js
+++ src/__tests__/tdd-markup.js
@@ -1,15 +1,23 @@
 import React from 'react'
-import {render, fireEvent} from '@testing-library/react'
+import {render, fireEvent, wait} from '@testing-library/react'
+
+import {Redirect as MockRedirect} from 'react-router'
+
 import {savePost as mockSavePost} from '../api'
 import {Editor} from '../post-editor-markup'
-
 jest.mock('../api')
+jest.mock('react-router', () => {
+  return {
+    Redirect: jest.fn(() => null),
+  }
+})
 
-test('renders a form with title, content, tags, and a submit button', () => {
+test('renders a form with title, content, tags, and a submit button', async () => {
   mockSavePost.mockResolvedValueOnce()
   const fakeUser = {id: 'user-1'}
   const {getByLabelText, getByText} = render(<Editor user={fakeUser} />)
 
+  const preDate = new Date().getTime()
   const fakePost = {
     title: 'Test Title',
     content: 'test content',
@@ -25,7 +33,17 @@ test('renders a form with title, content, tags, and a submit button', () => {
   expect(submitButton).toBeDisabled()
   expect(mockSavePost).toHaveBeenCalledWith({
     ...fakePost,
+    date: expect.any(String),
     authorId: fakeUser.id,
   })
+
+  const date = new Date(mockSavePost.mock.calls[0][0].date).getTime()
+  const postDate = new Date().getTime()
+
+  expect(date).toBeGreaterThanOrEqual(preDate)
+  expect(date).toBeLessThanOrEqual(postDate)
   expect(mockSavePost).toHaveBeenCalledTimes(1)
+
+  await wait(() => expect(MockRedirect).toHaveBeenCalledWith({to: '/'}, {}))
+  expect(MockRedirect).toHaveBeenCalledTimes(1)
 })
--- src/post-editor-markup.js
+++ src/post-editor-markup.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import {savePost} from './api'
+
+import {Redirect} from 'react-router'
 function Editor({user}) {
   const [isSaving, setIsSaving] = React.useState(false)
+  const [redirect, setRedirect] = React.useState(false)
 
   function handleSubmit(e) {
     e.preventDefault()
     const {title, content, tags} = e.target.elements
     setIsSaving(true)
-    savePost({
+    const newPost = {
       title: title.value,
       content: content.value,
       tags: tags.value.split(',').map(t => t.trim()),
+      date: new Date().toISOString(),
       authorId: user.id,
-    })
+    }
+    savePost(newPost).then(() => setRedirect(true))
+  }
+  if (redirect) {
+    return <Redirect to="/" />
   }
 
   return (
