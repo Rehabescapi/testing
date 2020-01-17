8e100445a1a0acd4f7ad2ced90372226a6a1814c
rehabescapi <jasonmlehmann@gmail.com>
2020-01-16 18:00:36 -0600

    createstore works

--- src/__tests__/redux.js
+++ src/__tests__/redux.js
@@ -1,18 +1,30 @@
 import React from 'react'
-
+import {createStore} from 'redux'
 import {render, fireEvent} from '@testing-library/react'
 import {Provider} from 'react-redux'
 
-import {store} from '../redux-store'
+import {store as AppStore} from '../redux-store'
 import {Counter} from '../redux-counter'
-
+import {reducer} from '../redux-reducer'
 test('can render with redux with default', () => {
   const {getByLabelText, getByText} = render(
-    <Provider store={store}>
+    <Provider store={AppStore}>
       <Counter />
     </Provider>,
   )
 
   fireEvent.click(getByText('+'))
   expect(getByLabelText(/count/i)).toHaveTextContent('1')
+})
+
+test('can render with redux with', () => {
+  const store = createStore(reducer, {count: 3})
+  const {getByLabelText, getByText} = render(
+    <Provider store={store}>
+      <Counter />
+    </Provider>,
+  )
+
+  fireEvent.click(getByText('-'))
+  expect(getByLabelText(/count/i)).toHaveTextContent('2')
 })
